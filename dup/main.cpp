48ff8c07f4f6b2a0b0ab4b1ad6c8a2bb83eeec26
chenhongliang2008 <chen4815336@163.com>
2020-06-12 14:26:36 +0800

    redirect dup

---
+++ dup/main.cpp
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+    int fd = 0;
+    char *buf="This is a test!!!!\n";
+	FILE *oldfd = fopen("mine.txt", "wb+");
+    if(oldfd == NULL)
+    {
+        printf("open error\n");
+        exit(-1);
+    }
+	int tmp  = fileno(oldfd);
+	int fdstdout = fileno(stdout);
+	fd = dup2(tmp, fdstdout);
+	if (fd == -1)
+	{
+		printf("dup2 out fail\n");
+		exit(-1);
+	}
+	int fdstderr = fileno(stderr);
+	fd = dup2(tmp, fdstderr);
+	if (fd == -1)
+	{
+		printf("dup2 err fail\n");
+		exit(-1);
+	}
+	
+	printf("test printf 1\n");
+    printf("dup2的返回值：%d\n",fd);
+
+	fprintf(stderr, buf);
+	fprintf(stdout, "test stdout 1\n");
+	fprintf(stderr, "test stderr 1\n");
+	fflush(oldfd);
+	fclose(oldfd);
+	return 0;
+}
